1427bf88ed56372d84f1c6513c454cc2eeb71daf
Nathanlsch <82216008+Nathanlsch@users.noreply.github.com>
2026-08-09 11:30:46 +0200

    Log Supabase URL and client details
    Added console logs to display Supabase URL and client information.

--- supabase.js
+++ supabase.js
@@ -1,7 +1,12 @@
 const SUPABASE_URL = "https://ffrsqutaqqxnfmkjnzhg.supabase.co";
 const SUPABASE_KEY = "sb_publishable_anU48SsDFt5QZthETgtHhg_FrGEb3Zs";
 
+console.log("URL utilisée :", SUPABASE_URL);
+
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+console.log("Client :", supabaseClient);
+console.log("Auth URL :", supabaseClient.auth.url);
