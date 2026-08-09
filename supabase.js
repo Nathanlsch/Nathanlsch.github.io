66ec3576b9ced7b7628791d36835f53c4c6a07d2
Nathanlsch <82216008+Nathanlsch@users.noreply.github.com>
2026-08-09 11:20:42 +0200

    Initialize Supabase client with URL and key

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ffrsqutaqqxnfmkjnzhg.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_anU48SsDFt5QZthETgtHhg_FrGEb3Zs";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
