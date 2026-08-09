60674c7ed45d0efad1f0e957b2325b32c3b6409e
Nathanlsch <82216008+Nathanlsch@users.noreply.github.com>
2026-08-09 11:26:25 +0200

    Update Supabase URL to remove endpoint path

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://ffrsqutaqqxnfmkjnzhg.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ffrsqutaqqxnfmkjnzhg.supabase.co";
 const SUPABASE_KEY = "sb_publishable_anU48SsDFt5QZthETgtHhg_FrGEb3Zs";
 
 const supabaseClient = window.supabase.createClient(
